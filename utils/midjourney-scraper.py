31804e1743e1c2614224043927b94a0241e8c618
rbbrdckybk <80607958+rbbrdckybk@users.noreply.github.com>
2023-06-27 11:54:52 -0400

    added ability to use local .json file as input

--- utils/midjourney-scraper.py
+++ utils/midjourney-scraper.py
@@ -15,7 +15,11 @@ import json
 import argparse
 
 url = "https://www.midjourney.com/showcase/recent/"
+search = 'recent.json'
+json_file = ''
+
 top = False
+searchFound = False
 
 # entry point
 if __name__ == '__main__':
@@ -25,8 +29,59 @@ if __name__ == '__main__':
         action='store_true',
         help="fetch the top showcase instead of recent"
     )
+    parser.add_argument(
+        "--file",
+        default='',
+        help="use this .json file instead of scraping midjourney.com"
+    )
     opt = parser.parse_args()
     top = opt.top
+    json_file = opt.file
+    
+    
+def create_output(json):
+    r = json
+    count = 0
+    prompts = []
+
+    if r["pageProps"]["jobs"]:
+        for job in r["pageProps"]["jobs"]:
+            if "prompt" in job and job["prompt"] is not None:
+                count += 1
+                prompt = job["prompt"].replace('\n', '').strip()
+                if prompt.startswith('['):
+                    prompt = 'image of ' + prompt
+                if '--ar' in prompt:
+                    prompt = prompt.split('--ar', 1)[0].strip()
+                while prompt.endswith(','):
+                    prompt = prompt[:-1]
+                while prompt.startswith('*'):
+                    prompt = prompt[1:]
+                if prompt.lower().startswith('/imagine'):
+                    prompt = prompt[8:].strip()
+                if '<<<' in prompt and '>>>' in prompt:
+                    start = prompt.split('<<<', 1)[0]
+                    end = prompt.split('>>>', 1)[1]
+                    prompt = start + ' ' + end
+                prompt = prompt.strip()
+                prompts.append(prompt)
+
+        prompts = list(set(prompts))
+        prompts.sort()
+
+        date = datetime.today().strftime('%Y-%m-%d')
+        filename = 'midjourney-' + date + '.prompts'
+        if top:
+            filename = 'midjourney-showcase-top.prompts'
+        with open(filename, 'w', encoding="utf-8") as f:
+            f.write('[config]\n\n!MODE = standard\n!REPEAT = yes\n\n!WIDTH = 1024\n!HEIGHT = 1024\n')
+            f.write('!HIGHRES_FIX = yes\n!STRENGTH = 0.68\n!FILENAME = <model>-<date>-<time>\n')
+            f.write('!AUTO_INSERT_MODEL_TRIGGER = end\n\n!CKPT_FILE = # add model(s) here')
+            f.write('\n\n[prompts]\n\n')
+            for prompt in prompts:
+                f.write(prompt + '\n\n')
+                
+        print('Done! Wrote ' + filename)
 
 
 def run(playwright: Playwright) -> None:
@@ -43,9 +98,11 @@ def run(playwright: Playwright) -> None:
 with sync_playwright() as playwright:
     def handle_response(response):
         global top
+        global search
+        global searchFound
         # the endpoint we are insterested in
         #print(response.url)
-        search = 'recent.json'
+
         if top:
             search = 'top.json'
         if (search in response.url):
@@ -53,46 +110,20 @@ with sync_playwright() as playwright:
             #with open('midjourney.json', 'w') as f:
             #    f.write(resp)
 
+            searchFound = True
             r = response.json()
-            count = 0
-            prompts = []
-
-            if r["pageProps"]["jobs"]:
-                for job in r["pageProps"]["jobs"]:
-                    if "prompt" in job and job["prompt"] is not None:
-                        count += 1
-                        prompt = job["prompt"].replace('\n', '').strip()
-                        if prompt.startswith('['):
-                            prompt = 'image of ' + prompt
-                        if '--ar' in prompt:
-                            prompt = prompt.split('--ar', 1)[0].strip()
-                        while prompt.endswith(','):
-                            prompt = prompt[:-1]
-                        while prompt.startswith('*'):
-                            prompt = prompt[1:]
-                        if prompt.lower().startswith('/imagine'):
-                            prompt = prompt[8:].strip()
-                        if '<<<' in prompt and '>>>' in prompt:
-                            start = prompt.split('<<<', 1)[0]
-                            end = prompt.split('>>>', 1)[1]
-                            prompt = start + ' ' + end
-                        prompt = prompt.strip()
-                        prompts.append(prompt)
-
-                prompts = list(set(prompts))
-                prompts.sort()
-
-                date = datetime.today().strftime('%Y-%m-%d')
-                filename = 'midjourney-' + date + '.prompts'
-                if top:
-                    filename = 'midjourney-showcase-top.prompts'
-                with open(filename, 'w', encoding="utf-8") as f:
-                    f.write('[config]\n\n!MODE = standard\n!REPEAT = yes\n\n!WIDTH = 1024\n!HEIGHT = 1024\n')
-                    f.write('!HIGHRES_FIX = yes\n!STRENGTH = 0.68\n!FILENAME = <model>-<date>-<time>\n')
-                    f.write('!AUTO_INSERT_MODEL_TRIGGER = end\n\n!CKPT_FILE = # add model(s) here')
-                    f.write('\n\n[prompts]\n\n')
-                    for prompt in prompts:
-                        f.write(prompt + '\n\n')
+            create_output(r)
 
 
-    run(playwright)
+    # we're scraping from midjourney.com
+    if json_file == '':
+        run(playwright)
+        if not searchFound:
+            print('Error: no response with "' + search + '" in the URL!')
+      
+    # we have a .json file
+    else:
+        lines = ''
+        with open(json_file, 'r', encoding="utf-8") as f:
+            json_data = json.load(f)
+        create_output(json_data)
